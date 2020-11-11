b761e4e91fc4420e85d0d31b1637839e2189053c
Tavish <bit_zjm@hotmail.com>
2020-11-11 18:31:48 +0800

    code format

--- src/main/java/pers/tavish/leetcode/easy/LongestCommonPrefix.java
+++ src/main/java/pers/tavish/leetcode/easy/LongestCommonPrefix.java
@@ -54,22 +54,22 @@ public class LongestCommonPrefix {
 //        return str1.substring(0, length);
 //    }
 
-     public String longestCommonPrefix(String[] strs) {
+    public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) return "";
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++)
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
-            if (prefix.isEmpty()) {
-                return "";
+                if (prefix.isEmpty()) {
+                    return "";
+                }
             }
-        }
         return prefix;
     }
 
     @Test
     public void testCase() {
-        String[] strs = {"flower","flow","flight"};
+        String[] strs = {"flower", "flow", "flight"};
         Assert.assertEquals("fl", longestCommonPrefix(strs));
     }
 
